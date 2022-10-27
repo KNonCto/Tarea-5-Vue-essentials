7b0466a6ee0ecad5dc97a5d32b741e6a4e911388
Daniel Parra <danielparraalvarez@gmail.com>
2022-10-26 22:25:51 -0300

    Names Working

--- tarea5/src/App.vue
+++ tarea5/src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/img/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/img/logo.png" />
+  <UserInfo v-for="user in users" :user="user" :key="user.id.value" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import UserInfo from "./components/UserInfo.vue";
+import Users from "./assets/json/users.json";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    UserInfo,
+  },
+  data() {
+    return {
+      users: Users,
+    };
+  },
+};
 </script>
 
 <style>
--- tarea5/src/components/UserInfo.vue
+++ tarea5/src/components/UserInfo.vue
@@ -1,30 +1,30 @@
 <template>
-    
-    </template>
-  
-  <script>
-  export default {
-    name: 'HelloWorld',
-    props: {
-      msg: String
-    }
-  }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-  </style>
+  <h3>{{ user.name.title }} {{ user.name.first }} {{ user.name.last }}</h3>
+</template>
+
+<script>
+export default {
+  name: "UserInfo",
+  props: {
+    user: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
